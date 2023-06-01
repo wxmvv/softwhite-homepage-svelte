73b715e2a7e981679e6c002d118c09fe3452bdd4
wxm <119100126+wxmvv@users.noreply.github.com>
2023-06-01 12:44:17 +0800

    fix

--- src/routes/program/+page.svelte
+++ src/routes/program/+page.svelte
@@ -30,10 +30,10 @@ import { base } from '$app/paths'
 		</article>
 	</main>
 	<!--  aria-hidden="true" -->
-	<div class="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6">
+	<div class="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-xl">
 		<div
-			class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+			class="aspect-[1100/678] w-[72rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-40"
+			style="clip-path: polygon(74% 44%, 100% 61.6%, 130% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 		/>
 	</div>
 </div>
