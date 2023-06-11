4cde4f8ce6732ba4853627bfde2e8223e6317ee9
wxm <119100126+wxmvv@users.noreply.github.com>
2023-06-11 16:32:43 +0800

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 	import '../app.css';
 	import NavBar from './components/NavBar.svelte';
 	import Footer from './components/Footer.svelte';
+
 	//TODO: 淡入淡出效果
 	//https://svelte.dev/repl/daa482983cd84a199f34895cdd3a08f6?version=3.38.2
-	
 </script>
 
 <NavBar />
--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -4,8 +4,8 @@
 <footer class="footer footer-center p-8 text-base-content rounded">
 	<div>
 		<div class="flex flex-row gap-6">
-			<a class="link link-hover" href="{base}/about">About us</a>
-			<a class="link link-hover" href="{base}/contact">Contact</a>
+			<a class="link link-hover" href="{base}/about">about us</a>
+			<a class="link link-hover" href="{base}/contact">contact</a>
 		</div>
 		<p>Copyright © 2023 - All right reserved by 舒白文化</p>
 	</div>
