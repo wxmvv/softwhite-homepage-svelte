2e0ab6b36d6b717cb0b33caa73d7f390a9786276
wxm <119100126+wxmvv@users.noreply.github.com>
2023-06-12 11:55:34 +0800

    font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,12 @@
 	<div class="flex flex-col justify-center h-screen">
 		<div class="logo_cn self-center relative top-12 right-1 text-7xl text-white">舒白</div>
 		<div class="logo_cn self-center relative bottom-6 text-7xl text-main">舒白</div>
-		<div class="logo_en self-center relative bottom-6 text-3xl text-main">Soft White</div>
+		<div class="self-center relative bottom-6 text-3xl text-main">Soft White</div>
 	</div>
 </main>
 
 <style>
 	.logo_cn {
 		font-family: 'PingFang';
-	}
-	.logo_en {
-		font-family: 'SFCompactRounded';
 	}
 </style>
